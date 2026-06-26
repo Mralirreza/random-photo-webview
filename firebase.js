d143f3f15ad7432ee29552b8b5573e386dd66416
alireza <96713054+Mralirreza@users.noreply.github.com>
2026-06-26 17:32:26 +0330

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -55,7 +55,7 @@ from
 
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-console.log(firebaseConfig);
+
 // ======================================
 // Firebase Config
 // ======================================
